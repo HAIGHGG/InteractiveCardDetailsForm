dbb70522a03bbf090724462b8fead9bbcf7418d9
Piotr <korotko.piotr@gmail.com>
2022-09-05 11:33:02 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,81 @@
+const inputName = document.querySelector('#name')
+const inputNumber = document.querySelector('#card-number')
+const inputDateMM = document.querySelector('#date-mm')
+const inputDateYY = document.querySelector('#date-yy')
+const inputCVC = document.querySelector('#cvc')
+const nameCard = document.querySelector('.name')
+const numberCard = document.querySelector('.top-card-number')
+const dateCard = document.querySelector('.date')
+const cvcCard = document.querySelector('.bottom-card-cvc')
+const inputs = document.querySelectorAll('input')
+const btnConfirm = document.querySelector('.btn-confirm')
+const popup = document.querySelector('.popup')
+const panel = document.querySelector('.panel')
+
+
+const updateCardNumber = () => {
+	const numbers = inputNumber.value.padEnd(16, '0')
+	numberCard.textContent = `${numbers.slice(0, 4)} ${numbers.slice(4, 8)} ${numbers.slice(8, 12)} ${numbers.slice(12)}`
+}
+
+const updateCardName = () => {
+	inputName.value === '' ? (nameCard.textContent = 'Jane Appleseed') : (nameCard.textContent = inputName.value)
+}
+
+const updateCardDate = () => {
+	dateCard.textContent = `${inputDateMM.value.padEnd(2, '0')}/${inputDateYY.value.padEnd(2, '0')}`
+}
+const updateCardCVC = () => {
+	inputCVC.value === '' ? (cvcCard.textContent = '000') : (cvcCard.textContent = inputCVC.value)
+}
+
+const isBlank = () => {
+	inputs.forEach(input => {
+		if (input.value === '') {
+			input.classList.add('input-error')
+			input.closest('div').querySelector('.error-msg').textContent = "Can't be blank"
+		} else {
+			input.classList.remove('input-error')
+			input.closest('div').querySelector('.error-msg').textContent = ''
+		}
+	})
+}
+
+const isCardNumberProper = () => {
+	if (inputNumber.value.length < 16 && inputNumber.value.length != 0) {
+		inputNumber.closest('div').querySelector('.error-msg').textContent = 'Wrong format, need 16 digits'
+	}
+}
+
+const confimation = () => {
+    const errorMsgs = document.querySelectorAll('.error-msg')
+    let errors = 0
+    errorMsgs.forEach(msg => {
+        if (msg.textContent != ''){
+            errors++
+        }
+    });
+    if (errors === 0){
+        panel.classList.add('hidden')
+        popup.classList.add('active')
+    }
+}
+
+inputName.addEventListener('keyup', updateCardName)
+inputNumber.addEventListener('keyup', updateCardNumber)
+inputCVC.addEventListener('keyup', updateCardCVC)
+inputDateMM.addEventListener('keyup', updateCardDate)
+inputDateYY.addEventListener('keyup', updateCardDate)
+btnConfirm.addEventListener('click', isBlank)
+btnConfirm.addEventListener('click', isCardNumberProper)
+btnConfirm.addEventListener('click', confimation)
+
+
+
+
+const onlyNumberKey = e => {
+	var ASCIICode = e.which ? e.which : e.keyCode
+	if (ASCIICode > 31 && (ASCIICode < 48 || ASCIICode > 57)) return false
+	return true
+}
+
